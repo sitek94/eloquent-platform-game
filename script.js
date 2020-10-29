e0eb7d3b67511febe402eecbbab1be15644300e6
sitek94 <msitkowski94@gmail.com>
2020-10-29 10:34:43 +0100

    Define `levelChars`
    Object that maps plan characters to eigher background grid types or actor classes.

---
+++ script.js
@@ -0,0 +1,71 @@
+let Player, Coin, Lava;
+
+const levelChars = {
+  // Empty space
+  '.': 'empty',
+  // Walls
+  '#': 'wall',
+  // Lava
+  '+': 'lava',
+  // Player's starting position
+  '@': Player,
+  // Coins
+  o: Coin,
+  // Block of lava that moves back and forth horizontally
+  '=': Lava,
+  // Vertically moving blobs
+  '|': Lava,
+  // Dripping lava - vertically moving lava that doesn’t bounce back and forth but only
+  // moves down, jumping back to its start position when it hits the floor.
+  v: Lava,
+};
+
+let simpleLevelPlan = `
+......................
+..#................#..
+..#..............=.#..
+..#.........o.o....#..
+..#.@......#####...#..
+..#####............#..
+......#++++++++++++#..
+......##############..
+......................`;
+
+/* 
+A whole game consists of multiple levels that the player must complete. A level is completed when all coins have been collected. If the player touches lava, the current level is restored to its starting position, and the player may try again.
+*/
+
+/* READING A LEVEL */
+
+class Level {
+  constructor(plan) {
+    // Rows of the plan
+    let rows = plan
+      // Remove whitespace so that we can start with new line.
+      .trim()
+      .split('\n')
+      // Spread each line into array array of characters
+      .map(line => [...line]);
+
+    this.height = rows.length;
+    this.width = rows[0].length;
+
+    // Actors - all moving elements
+    this.startActors = [];
+
+    
+    this.rows = rows.map((row, y) => {
+      return row.map((char, x) => {
+        let type = levelChars[char];
+
+        if (typeof type === 'string') return type;
+
+        this.startActors.push(
+          type.create(new Vec(x, y), char)
+        );
+
+        return 'empty';
+      });
+    });
+  }
+}
