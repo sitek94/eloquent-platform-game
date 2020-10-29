9810da1beb9b15bafc935c2f09b8518c4c9c4b1e
sitek94 <msitkowski94@gmail.com>
2020-10-29 10:46:18 +0100

    Create `Vec` class
    A class that is used for two-dimensional values, such as the position and size of actors.

--- script.js
+++ script.js
@@ -96,3 +96,22 @@ class State {
     return this.actors.find(actor => actor.type === "player");
   }
 }
+
+/* VEC - creates two-dimensional vector */
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y);
+  }
+
+  // Scales a vector by a given number. It will be useful when we need
+  // to multiply a speed vector by a time interval to get the distance
+  // during that time
+  times(factor) {
+    return new Vec(this.x * factor, this.y * factor);
+  }
+}
