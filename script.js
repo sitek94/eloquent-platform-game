93300c032b5416c53bd23281890478f8073e91f6
sitek94 <msitkowski94@gmail.com>
2020-11-04 10:41:19 +0100

    Finish exercise monster - my solution

--- script.js
+++ script.js
@@ -668,4 +668,63 @@ async function runGame(plans, Display) {
   }
 }
 
-runGame(GAME_LEVELS, DOMDisplay);
+class Monster {
+  constructor(pos, speed) {
+    this.pos = pos;
+    this.speed = speed;
+  }
+
+  get type() {
+    return 'monster';
+  }
+
+  static create(pos) {
+    return new Monster(pos.plus(new Vec(0, -1)), new Vec(2, 0));
+  }
+
+  update(time, state) {
+    let newPos = this.pos.plus(this.speed.times(time));
+
+    if (!state.level.touches(newPos, this.size, 'wall')) {
+      return new Monster(newPos, this.speed);
+    } else {
+      return new Monster(this.pos, this.speed.times(-1));
+    }
+  }
+
+  collide(state) {
+    
+    let player = state.actors.find(a => a.type === 'player');
+    
+    let isAbove = player.pos.y + player.size.y < this.pos.y + .2; 
+    
+    let filtered = state.actors.filter(a => a !== this);
+
+    if (isAbove) {
+      return new State(state.level, filtered, state.status);
+    } else {
+      return new State(state.level, state.actors, 'lost');
+    }
+  }
+}
+
+Monster.prototype.size = new Vec(1.2, 2);
+
+levelChars['M'] = Monster;
+
+// runGame(GAME_LEVELS, DOMDisplay);
+
+runGame([`
+..................................
+.################################.
+.#..............................#.
+.#..............................#.
+.#..............................#.
+.#...........................o..#.
+.#..@...........................#.
+.##########..............########.
+..........#..o..o..o..o..#........
+..........#..........M...#........
+..........################........
+..................................
+`], DOMDisplay);
